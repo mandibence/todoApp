fb61ae5b4eacbc42b4d36840cebb1fdc60d66b45
Bence Mandi <mandi.bence@gmail.com>
2024-05-10 14:40:03 +0200

    working on the styling of the todoList 2

--- myapp/src/components/Header.js
+++ myapp/src/components/Header.js
@@ -4,7 +4,7 @@ import "../css/Header.css"
 export default function Header() {
     return (
         <div className="header">
-            <div className="header-text">Todo App</div>
+            <div className="header-text">TODO APP</div>
         </div>
 
     )
